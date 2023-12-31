800d271eeac31be9381615593ff6fc15d523ffd2
Cristiano Prato <cristiano.prato@egluetechnologies.com>
2023-12-31 12:21:38 +0100

    complete day13

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,5 @@
 use grid::Grid;
+use itertools::Itertools;
 
 fn parse(input: &str) -> Vec<Grid<char>> {
     let mut grids = Vec::new();
@@ -31,134 +32,148 @@ fn parse(input: &str) -> Vec<Grid<char>> {
     grids
 }
 
-fn is_symmetric_horizontally(g: &Grid<char>, a: usize, b: usize) -> bool{
-    let mut a = a;
-    let mut b = b;
-    while a < b {
-        let r1: Vec<&char> = g.iter_rows().nth(a).unwrap().collect();
-        let r2: Vec<&char> = g.iter_rows().nth(b).unwrap().collect();
-        if r1 != r2 {
-            return false;
-        }
-        a += 1;
-        b -= 1;
+fn _show(g: &Grid<char>) {
+    println!("");
+    print!("     ");
+    for c in 0..g.cols() {
+        print!("{}", c%10);
     }
-    true
-}
-
-fn is_symmetric_vertically(g: &Grid<char>, a: usize, b: usize) -> bool{
-    let mut a = a;
-    let mut b = b;
-    while a < b {
-        let c1: Vec<&char> = g.iter_cols().nth(a).unwrap().collect();
-        let c2: Vec<&char> = g.iter_cols().nth(b).unwrap().collect();
-        if c1 != c2 {
-            return false;
+    println!("");
+    for (r, i) in g.iter_rows().enumerate() {
+        print!("{r:3}| ");
+        for e in i.into_iter() {
+            print!("{e}");
         }
-        a += 1;
-        b -= 1;
+        println!("");
     }
-    true
-}
-
-// fn show(g: &Grid<char>) {
-//     println!("");
-//     print!("     ");
-//     for c in 0..g.cols() {
-//         print!("{}", c%10);
-//     }
-//     println!("");
-//     for (r, i) in g.iter_rows().enumerate() {
-//         print!("{r:3}| ");
-//         for e in i.into_iter() {
-//             print!("{e}");
-//         }
-//         println!("");
-//     }
-// }
-
-fn solve_horizontal_remove_top(g: &Grid<char>) -> usize {
-    let mut res = 0;
-    let mut n = 0;
-    for i in 0..g.rows()-1 {
-        if i%2 == 0 {
-            n += 1;
-            continue;
-        }
-        if is_symmetric_horizontally(g, i, g.rows()-1) {
-            res = ((g.rows()-n)/2 + n) * 100;
-        }
-        n += 1;
-    }
-    res
-}
-
-fn solve_vertical_remove_left(g: &Grid<char>) -> usize {
-    let mut res = 0;
-    let mut n = 0;
-    for i in 0..g.cols()-1 {
-        if i%2 == 0 {
-            n += 1;
-            continue;
-        }
-        if is_symmetric_vertically(g, i, g.cols()-1) {
-            res = ((g.cols()-n)/2 + n) * 1;
-        }
-        n += 1;
-    }
-    res
-}
-
-fn solve_vertical_remove_right(g: &Grid<char>) -> usize {
-    let mut res = 0;
-    let mut n = 0;
-    let mut i = g.cols()-1;
-    while i > 0 {
-        if i%2 == 0 {
-            n += 1;
-            i -= 1;
-            continue;
-        }
-        if is_symmetric_vertically(g, 0, i) {
-            res = ((g.cols()-n)/2) * 1;
-        }
-        n += 1;
-        i -= 1;
-    }
-    res
-}
-
-fn solve_horizontal_remove_bottom(g: &Grid<char>) -> usize {
-    let mut res = 0;
-    let mut n = 0;
-    let mut i = g.rows()-1;
-    while i > 0 {
-        if i%2 == 0 {
-            n += 1;
-            i -= 1;
-            continue;
-        }
-        if is_symmetric_horizontally(g, 0, i) {
-            res = ((g.rows()-n)/2) * 100;
-        }
-        n += 1;
-        i -= 1;
-    }
-    res
 }
 
 fn solve(grids: &Vec<Grid<char>>) -> usize {
     grids
     .iter()
     .map(|g| {
-        let mut res = solve_horizontal_remove_top(g);
-        res += solve_vertical_remove_left(g);
-        res += solve_vertical_remove_right(g);
-        res += solve_horizontal_remove_bottom(g);
-        if res == 0 {
-            panic!("no symmetry found");
+        for ((r1, a), (r2, b)) in g
+        .iter_rows()
+        .enumerate()
+        .tuple_windows() {
+            let different_elements = a.zip(b).filter(|(e1, e2)| e1 != e2).count();
+            if different_elements == 0 {
+                let mut i = r1;
+                let mut j = r2;
+                let mut symmetric = true;
+                while i > 0 && j < g.rows()-1 {
+                    i -= 1;
+                    j += 1;
+                    if g.iter_rows().nth(i).unwrap().collect::<Vec<&char>>() != g.iter_rows().nth(j).unwrap().collect::<Vec<&char>>() {
+                        symmetric = false;
+                        break;
+                    }
+                }
+                if symmetric {
+                    return (r1 + 1) * 100;
+                }
+            }
         }
-        res
+
+        for ((c1, a), (c2, b)) in g
+        .iter_cols()
+        .enumerate()
+        .tuple_windows() {
+            let different_elements = a.zip(b).filter(|(e1, e2)| e1 != e2).count();
+            if different_elements == 0 {
+                let mut i = c1;
+                let mut j = c2;
+                let mut symmetric = true;
+                while i > 0 && j < g.cols()-1 {
+                    i -= 1;
+                    j += 1;
+                    if g.iter_cols().nth(i).unwrap().collect::<Vec<&char>>() != g.iter_cols().nth(j).unwrap().collect::<Vec<&char>>() {
+                        symmetric = false;
+                        break;
+                    }
+                }
+                if symmetric {
+                    return c1 + 1;
+                }
+            }
+        }
+        
+        panic!("no symmetry found");
+    })
+    .sum()
+}
+
+fn solve2(grids: &Vec<Grid<char>>) -> usize {
+    grids
+    .iter()
+    .map(|g| {
+        for ((r1, a), (r2, b)) in g
+        .iter_rows()
+        .enumerate()
+        .tuple_windows() {
+            let mut smudge_used = false;
+            let different_elements = a.zip(b).filter(|(e1, e2)| e1 != e2).count();
+            if different_elements == 1 {
+                smudge_used = true;
+            }
+            if different_elements == 0 || smudge_used {
+                let mut i = r1;
+                let mut j = r2;
+                let mut symmetric = true;
+                while i > 0 && j < g.rows()-1 {
+                    i -= 1;
+                    j += 1;
+                    let different_elements_in_row = g.iter_rows().nth(i).unwrap().zip(g.iter_rows().nth(j).unwrap()).filter(|(e1, e2)| e1 != e2).count();
+                    if different_elements_in_row == 1 && !smudge_used {
+                        smudge_used = true;
+                        continue;
+                    }
+                    if different_elements_in_row == 0 {
+                        continue;
+                    }
+                    symmetric = false;
+                    break;   
+                }
+                if symmetric && smudge_used {
+                    return (r1 + 1) * 100;
+                }
+            }
+        }
+
+        for ((c1, a), (c2, b)) in g
+        .iter_cols()
+        .enumerate()
+        .tuple_windows() {
+            let mut smudge_used = false;
+            let different_elements = a.zip(b).filter(|(e1, e2)| e1 != e2).count();
+            if different_elements == 1 {
+                smudge_used = true;
+            }
+            if different_elements == 0 || smudge_used {
+                let mut i = c1;
+                let mut j = c2;
+                let mut symmetric = true;
+                while i > 0 && j < g.cols()-1 {
+                    i -= 1;
+                    j += 1;
+                    let different_elements_in_col = g.iter_cols().nth(i).unwrap().zip(g.iter_cols().nth(j).unwrap()).filter(|(e1, e2)| e1 != e2).count();
+                    if different_elements_in_col == 1 && !smudge_used {
+                        smudge_used = true;
+                        continue;
+                    }
+                    if different_elements_in_col == 0 {
+                        continue;
+                    }
+                    symmetric = false;
+                    break;   
+                }
+                if symmetric && smudge_used {
+                    return c1 + 1;
+                }
+            }
+        }
+        
+        panic!("no symmetry found");
     })
     .sum()
 }
@@ -167,6 +182,12 @@ pub fn part1() {
     let input = std::fs::read_to_string("data/day13/input2.txt").unwrap();
     let grids = parse(&input);
     println!("Day 13, Part 1: {}", solve(&grids));
+}
+
+pub fn part2() {
+    let input = std::fs::read_to_string("data/day13/input2.txt").unwrap();
+    let grids = parse(&input);
+    println!("Day 13, Part 2: {}", solve2(&grids));
 }
 
 mod tests {
@@ -184,5 +205,21 @@ mod tests {
         let grids = super::parse(&input);
         let res = super::solve(&grids);
         assert_eq!(43614, res);
+    }
+
+    #[test]
+    fn t3() {
+        let input = std::fs::read_to_string("data/day13/input1.txt").unwrap();
+        let grids = super::parse(&input);
+        let res = super::solve2(&grids);
+        assert_eq!(400, res);
+    }
+
+    #[test]
+    fn t4() {
+        let input = std::fs::read_to_string("data/day13/input2.txt").unwrap();
+        let grids = super::parse(&input);
+        let res = super::solve2(&grids);
+        assert_eq!(36771, res);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,7 @@ fn main() {
         },
         13 => {
             day13::part1();
+            day13::part2();
         },
         _ => {
             unimplemented!();
