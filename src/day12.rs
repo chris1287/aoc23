2670ca8f53dc6c8476ae789044b89ce357653af2
Cristiano Prato <cristiano.prato@egluetechnologies.com>
2024-01-02 14:07:39 +0100

    complete day12 (finally)

--- src/day12.rs
+++ src/day12.rs
@@ -1,6 +1,6 @@
-use core::panic;
-use std::collections::VecDeque;
-use rayon::prelude::*;
+use std::collections::HashMap;
+
+type Cache = HashMap::<(String, Vec<usize>), usize>;
 
 #[derive(Debug, Default)]
 struct Record {
@@ -23,110 +23,142 @@ fn parse(input: &str) -> Vec<Record> {
     records
 }
 
-fn is_combination_valid(combination: &str, groups: &Vec<usize>) -> bool {
-    let mut current_group_len = 0;
-    let mut in_a_group = false;
-    let mut partial_combination = false;
-    let mut wannabe_group = Vec::new();
-    let mut current_group_id : i32 = -1;
-    for c in combination.chars() {
-        match c {
-            '.' => {
-                // Functional
-                if in_a_group {
-                    in_a_group = false;
-                    wannabe_group.push(current_group_len);
-                    current_group_len = 0;
-                }
-            },
-            '#' => {
-                // Not functional
-                if !in_a_group {
-                    current_group_id += 1;
-                    in_a_group = true;
-                }
-                current_group_len += 1;
-            },
-            _ => {
-                partial_combination = true;
-                if in_a_group {
-                    if current_group_id as usize >= groups.len() {
-                        // Cannot be valid
-                        return false;
-                    }
-                    if current_group_id >= 0 && current_group_len > groups[current_group_id as usize] {
-                        // Cannot be valid
-                        return false;
-                    } else {
-                        // Cannot decide yet, maybe valid
-                        in_a_group = false;
-                        wannabe_group.pop();
-                    }
-                }
-                // Cannot decide yet, maybe valid
-                break;
-            },
-        }
-    }
-    if in_a_group {
-        wannabe_group.push(current_group_len);
-    }
+fn parse2(input: &str) -> Vec<Record> {
+    let mut records = Vec::new();
+    for line in input.lines() {
+        let mut v = line.split(" ");
+        let seed = v.next().expect("should have seed");
+        let groups = v.next().expect("should have groups");
+        let groups = format!("{},{},{},{},{}", groups, groups, groups, groups, groups);
+        let groups = groups.split(",").map(|s| s.parse::<usize>().expect("should be a number")).collect::<Vec<_>>();
 
-    if wannabe_group.len() > groups.len() {
-        // Cannot be valid
-        return false;
-    }
+        let seed = format!("{}?{}?{}?{}?{}", seed, seed, seed, seed, seed);
 
-    if partial_combination {
-        for (i, g) in wannabe_group.iter().enumerate() {
-            if *g != groups[i] {
-                // Cannot be valid
-                return false;
-            }
-        }
-        // Might be valid
-        return true;
-    } else {
-        // Must be exactly equal
-        return wannabe_group == *groups;
+        records.push(Record {
+            seed: seed.to_string(),
+            groups,
+        });
     }
-}
-
-fn generate_arrangements(record: &mut Record) -> usize {
-    let mut arrangements = VecDeque::new();
-    arrangements.push_front(record.seed.clone());
-    for (i, _e) in record.seed.chars().enumerate() {
-        let elements = arrangements.len();
-        for _j in 0..elements {
-            let r = arrangements.pop_back().unwrap();
-            match r.chars().nth(i) {
-                Some('.') | Some('#') => arrangements.push_front(r),
-                Some('?') => {
-                    let mut r1 = r.clone();
-                    r1.replace_range(i..i+1, ".");
-                    let mut r2 = r.clone();
-                    r2.replace_range(i..i+1, "#");
-                    if is_combination_valid(&r1, &record.groups) {
-                        arrangements.push_front(r1);
-                    }
-                    if is_combination_valid(&r2, &record.groups) {
-                        arrangements.push_front(r2);
-                    }
-                },
-                _ => panic!("Should not happen"),
-            }
-        }
-    }
-    arrangements.len()
+    records
 }
 
 fn solve(records: &mut Vec<Record>) -> usize {
     records
-    .par_iter_mut()
+    // .par_iter_mut()
+    .iter_mut()
     .map(|record|{
-        generate_arrangements(record)
+        let mut memo = Cache::new();
+        recurse(&record.seed, &record.groups, &mut memo)
     })
     .sum()
+}
+
+fn on_dot(
+    seed: &str,
+    groups: &[usize],
+    cache: &mut Cache,
+) -> usize {
+    if seed.len() >= 1 {
+        let res = recurse(&seed[1..], groups, cache);
+        cache.insert((seed[1..].to_owned(), groups.to_vec()), res);
+        return res;
+    }
+
+    if groups.len() == 0 {
+        cache.insert((seed.to_owned(), groups.to_vec()), 1);
+        return 1;
+    }
+
+    cache.insert((seed.to_owned(), groups.to_vec()), 0);
+    return 0;
+}
+
+fn on_hash(
+    seed: &str,
+    groups: &[usize],
+    cache: &mut Cache,
+) -> usize {
+
+    // # ()
+    if groups.len() == 0 {
+        cache.insert((seed.to_owned(), groups.to_vec()), 0);
+        return 0;
+    }
+
+    // ## (4)
+    if groups.iter().sum::<usize>() > seed.len() {
+        // no way to fit a group here
+        cache.insert((seed.to_owned(), groups.to_vec()), 0);
+        return 0;
+    }
+
+    // ##.# (4)
+    if seed[0..groups[0]].chars().into_iter().any(|c| c == '.') {
+        // There is a dot in the group, no way to fit
+        cache.insert((seed.to_owned(), groups.to_vec()), 0);
+        return 0;
+    }
+
+    // #### (3)
+    if seed.len() > groups[0] && seed.chars().nth(groups[0]).unwrap()  == '#' {
+        // Group is not closed, invalid config
+        cache.insert((seed.to_owned(), groups.to_vec()), 0);
+        return 0;
+    }
+
+    // ## (3)
+    if groups.len() == 1 {
+        if seed.chars().count() < groups[0] {
+            cache.insert((seed.to_owned(), groups.to_vec()), 0);
+            return 0;
+        }
+    }
+
+    // ###.## (3, 2)
+    if seed.len() > groups[0] {
+        let res = recurse(&seed[groups[0]+1..], &groups[1..], cache);
+        cache.insert((seed[groups[0]+1..].to_owned(), groups[1..].to_vec()), res);
+        return res;
+    }
+
+    // ### (3)
+    cache.insert((seed.to_owned(), groups.to_vec()), 1);
+    return 1
+}
+
+fn recurse(
+    seed: &str,
+    groups: &[usize],
+    cache: &mut Cache,
+) -> usize {
+
+    let k = (seed.to_owned(), groups.to_vec());
+    if cache.contains_key(&k) {
+        return cache.get(&k).unwrap().clone();
+    }
+
+    match seed.chars().next() {
+        Some('.') => {
+            return on_dot(seed, groups, cache);
+        },
+        Some('#') => {
+            return on_hash(seed, groups, cache);
+        },
+        Some('?') => {
+            return
+                // consider this a '.': 
+                on_dot(seed, groups, cache) + 
+                // consider this a '#':
+                on_hash(seed, groups, cache);
+        },
+        None => {
+            if groups.len() == 0 {
+                return 1;
+            }
+            return 0;
+        }
+        _ => panic!("Invalid symbol"),
+    }
 }
 
 pub fn solve1() -> usize {
@@ -135,12 +167,18 @@ pub fn solve1() -> usize {
     solve(&mut records)
 }
 
+pub fn solve2() -> usize {
+    let input = std::fs::read_to_string("data/day12/input2.txt").unwrap();
+    let mut records = parse2(&input);
+    solve(&mut records)
+}
+
 pub fn part1() {
     println!("Day 12, Part 1: {}", solve1());
 }
 
 pub fn part2() {
-    println!("Day 12, Part 2: NA");
+    println!("Day 12, Part 2: {}", solve2());
 }
 
 mod tests {
@@ -163,7 +201,7 @@ mod tests {
     #[test]
     fn t3() {
         let input = std::fs::read_to_string("data/day12/input1.txt").unwrap();
-        let mut records = super::parse(&input);
+        let mut records = super::parse2(&input);
         let res = super::solve(&mut records);
         assert_eq!(res, 525152);
     }
@@ -171,16 +209,8 @@ mod tests {
     #[test]
     fn t4() {
         let input = std::fs::read_to_string("data/day12/input2.txt").unwrap();
-        let mut records = super::parse(&input);
+        let mut records = super::parse2(&input);
         let res = super::solve(&mut records);
-        assert_eq!(res, 0);
-    }
-
-    #[test]
-    fn t5() {
-        let input = std::fs::read_to_string("data/day12/input3.txt").unwrap();
-        let mut records = super::parse(&input);
-        let res = super::solve(&mut records);
-        assert_eq!(res, 0);
+        assert_eq!(res, 17485169859432);
     }
 }
